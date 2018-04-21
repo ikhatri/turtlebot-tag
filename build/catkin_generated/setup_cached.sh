cb968a9ff97d8c5e81209916b3d84ada124269f9
ikhatri <ishan@khatri.io>
2018-04-21 19:53:57 -0400

    Created a ros package

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ikhatri/Documents/CS403/kinetic_ws/turtlebot_tag/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ikhatri/Documents/CS403/kinetic_ws/turtlebot_tag/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ikhatri/Documents/CS403/kinetic_ws/turtlebot_tag:/opt/ros/kinetic/share"
