21b6cc20abe959e2a3ecaaed78f9ac452196caf5
ikhatri <ishan@khatri.io>
2018-04-21 20:08:16 -0400

    Added code stub

---
+++ src/turtlebot_tag.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <sstream>
+
+#include <std_msgs/String.h>
+
+#include "sensor_msgs/LaserScan.h"
+#include "sensor_msgs/PointCloud.h"
+#include "geometry_msgs/Point32.h"
+#include <math.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main(int argc, char **argv){
+    return 0;
+}
