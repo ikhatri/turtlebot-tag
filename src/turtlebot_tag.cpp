fa3f2fa23f48ef5eab57847d2c5eab6e360f7072
ikhatri <ishan@khatri.io>
2018-04-27 02:16:15 -0400

    Fixed compilation issues & added more stub code for kinect

--- src/turtlebot_tag.cpp
+++ src/turtlebot_tag.cpp
@@ -4,6 +4,10 @@
 
 #include <std_msgs/String.h>
 
+// #include <image_transport/image_transport.h>
+// #include <cv_bridge/cv_bridge.h>
+// #include <sensor_msgs/image_encodings.h>
+
 #include "sensor_msgs/LaserScan.h"
 #include "sensor_msgs/PointCloud.h"
 #include "geometry_msgs/Point32.h"
@@ -13,7 +17,17 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using ros::Subscriber;
+
+Subscriber points;
 
 int main(int argc, char **argv){
-    return 0;
+  ros::init(argc, argv, "turtlebot_tag");
+  ros::NodeHandle n;
+
+  //points = n.subscriber("camera/depth_register/points", callback_here);
+  // the callback_here() function takes in a point cloud
+
+  ros::spin();
+  return 0;
 }
