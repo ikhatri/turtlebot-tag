ebdf15bb8738fdd82ff0a028e0b759f6f9224631
ikhatri <ishan@khatri.io>
2018-05-03 08:55:08 -0400

    Added 3D blob detection & updated turtlebot_tag executable to support moving goal points from the blob detection

--- src/blob_detection.cpp
+++ src/blob_detection.cpp
@@ -7,6 +7,10 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/LaserScan.h"
@@ -104,13 +108,96 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg){
   cv::waitKey(3);
 }
 
+void callback(const sensor_msgs::ImageConstPtr& i1, const sensor_msgs::ImageConstPtr& i2){
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(i1, sensor_msgs::image_encodings::BGR8);
+  }
+  catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  cv::Mat orange_image;
+  cv::Scalar lower(0, 0, 55);
+  cv::Scalar upper(20, 20, 255);
+  cv::inRange(cv_ptr->image, lower, upper, orange_image);
+
+  // Setup SimpleBlobDetector parameters.
+  cv::SimpleBlobDetector::Params params;
+  
+  // Change thresholds
+  params.minThreshold = 10;
+  params.maxThreshold = 200;
+  
+  // Filter by Area.
+  params.filterByArea = true;
+  params.minArea = 10;
+  
+  // Filter by Circularity
+  params.filterByCircularity = true;
+  params.minCircularity = 0.3;
+  params.maxCircularity = 0.8;
+  
+  // Filter by Convexity
+  params.filterByConvexity = true;
+  params.minConvexity = 0.87;
+  
+  // Filter by Inertia
+  params.filterByInertia = true;
+  params.minInertiaRatio = 0.01;
+  
+
+  cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+  std::vector<cv::KeyPoint> keypoints;
+  detector->detect(orange_image, keypoints);
+  float x_total = 0;
+  float y_total = 0;
+  for(auto& k : keypoints){
+    x_total += k.pt.x;
+    y_total += k.pt.y;
+  }
+  std::vector<cv::KeyPoint> the_keypoint;
+  the_keypoint.push_back(cv::KeyPoint(x_total/keypoints.size(), y_total/keypoints.size(), 100));
+  cv::Mat im_with_keypoints;
+  cv::drawKeypoints( cv_ptr->image, the_keypoint, im_with_keypoints, cv::Scalar(0,255,0), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+  
+
+  // Encoding the depth image to be read in easily
+  cv_bridge::CvImagePtr cv_ptr2;
+  try {
+    cv_ptr2 = cv_bridge::toCvCopy(i2, sensor_msgs::image_encodings::TYPE_16UC1);
+  }
+  catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  float depth = cv_ptr2->image.at<short int>(cv::Point(x_total/keypoints.size(), y_total/keypoints.size()));
+
+  // Set the new goal point
+  // Need to check if this is actually correct
+  goal_[0] = x_total/keypoints.size();
+  goal_[1] = -1*y_total/keypoints.size();
+  goal_[2] = depth;
+  
+  cv::imshow(OPENCV_WINDOW, im_with_keypoints);
+  cv::waitKey(3);
+}
+
 int main(int argc, char **argv){
   ros::init(argc, argv, "turtlebot_tag");
   ros::NodeHandle n;
   image_transport::ImageTransport it_(n);
 
-  image_sub_ = it_.subscribe("/camera/rgb/image_color", 1, imageCb);
-  points = n.subscribe("/camera/depth_register/points", 1000, KinectCallback);
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_policy_classification;
+  message_filters::Subscriber<sensor_msgs::Image> image_sub_(n, "/camera/rgb/image_color", 10);
+  message_filters::Subscriber<sensor_msgs::Image> points(n, "/camera/depth/image", 10);
+  message_filters::Synchronizer<sync_policy_classification> sync(sync_policy_classification(10), image_sub_, points);
+  sync.registerCallback(boost::bind(&callback, _1, _2));
+
+  // image_sub_ = it_.subscribe("/camera/rgb/image_color", 1, imageCb);
+  // points = n.subscribe("/camera/depth_register/points", 1000, KinectCallback);
 
   cv::namedWindow(OPENCV_WINDOW);
   
--- src/turtlebot_tag.cpp
+++ src/turtlebot_tag.cpp
@@ -14,6 +14,17 @@
 #include <nav_msgs/Odometry.h>
 #include <visualization_msgs/Marker.h>
 
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include "turtlebot_tag/CheckPointSrv.h"
 #include "turtlebot_tag/GetFreePathSrv.h"
 #include "turtlebot_tag/ObstacleLaserScanSrv.h"
@@ -43,10 +54,10 @@ using turtlebot_tag::GetCommandVelSrv;
 using turtlebot_tag::GetTransformationSrv;
 
 // Services and Subscribers
-ros::ServiceServer checkPointServer;
-ros::ServiceServer getFreePathServer;
-ros::ServiceServer transformPointsServer;
-ros::ServiceServer getCommandVelocityServer;
+// ros::ServiceServer checkPointServer;
+// ros::ServiceServer getFreePathServer;
+// ros::ServiceServer transformPointsServer;
+// ros::ServiceServer getCommandVelocityServer;
 
 ros::Subscriber laserScanSubscriber;
 ros::Subscriber odometrySubscriber;
@@ -54,6 +65,8 @@ ros::ServiceClient getTransformClient;
 
 ros::Publisher driveMsgPublisher;
 ros::Publisher rvizPublisher;
+
+image_transport::Subscriber image_sub_;
 
 // Robot Params
 float robotRadius = 0.18; // 0.18 meters
@@ -548,11 +561,85 @@ void odometeryCallback(const Odometry& odom) {
   */
 }
 
+void callback(const sensor_msgs::ImageConstPtr& i1, const sensor_msgs::ImageConstPtr& i2){
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(i1, sensor_msgs::image_encodings::BGR8);
+  }
+  catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  cv::Mat orange_image;
+  cv::Scalar lower(0, 0, 55);
+  cv::Scalar upper(20, 20, 255);
+  cv::inRange(cv_ptr->image, lower, upper, orange_image);
+
+  // Setup SimpleBlobDetector parameters.
+  cv::SimpleBlobDetector::Params params;
+  
+  // Change thresholds
+  params.minThreshold = 10;
+  params.maxThreshold = 200;
+  
+  // Filter by Area.
+  params.filterByArea = true;
+  params.minArea = 10;
+  
+  // Filter by Circularity
+  params.filterByCircularity = true;
+  params.minCircularity = 0.3;
+  params.maxCircularity = 0.8;
+  
+  // Filter by Convexity
+  params.filterByConvexity = true;
+  params.minConvexity = 0.87;
+  
+  // Filter by Inertia
+  params.filterByInertia = true;
+  params.minInertiaRatio = 0.01;
+  
+
+  cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+  std::vector<cv::KeyPoint> keypoints;
+  detector->detect(orange_image, keypoints);
+  float x_total = 0;
+  float y_total = 0;
+  for(auto& k : keypoints){
+    x_total += k.pt.x;
+    y_total += k.pt.y;
+  }
+  std::vector<cv::KeyPoint> the_keypoint;
+  the_keypoint.push_back(cv::KeyPoint(x_total/keypoints.size(), y_total/keypoints.size(), 100));
+  // cv::Mat im_with_keypoints;
+  // cv::drawKeypoints( cv_ptr->image, the_keypoint, im_with_keypoints, cv::Scalar(0,255,0), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+  
+
+  // Encoding the depth image to be read in easily
+  cv_bridge::CvImagePtr cv_ptr2;
+  try {
+    cv_ptr2 = cv_bridge::toCvCopy(i2, sensor_msgs::image_encodings::TYPE_16UC1);
+  }
+  catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  float depth = cv_ptr2->image.at<short int>(cv::Point(x_total/keypoints.size(), y_total/keypoints.size()));
+
+  // Set the new goal point
+  // Need to check if this is actually correct
+  target.x = x_total/keypoints.size();
+  target.y = -1*y_total/keypoints.size();
+  target.z = depth;
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "assignment5");
   ros::NodeHandle n;
 
-  if (argc != 8) {
+  if (argc != 6) {
     ROS_INFO("LOL U DIDN'T GIVE ANY INPUT ARGS");
     ROS_INFO("CHECK MAIN TO SEE WHAT U NEED TO INPUT LOL");
     return 0;
@@ -562,15 +649,23 @@ int main(int argc, char **argv) {
   speedWeight = float(atof(argv[3])); // 1 // 150     // 50
   clearanceWeight = float(atof(argv[4])); // 0 // 10  // 1
   stopDistanceOffset = float(atof(argv[5]));         // 0.25
-  target.x = float(atof(argv[6]));
-  target.y = float(atof(argv[7]));
+  // target.x = float(atof(argv[6]));
+  // target.y = float(atof(argv[7]));
 
 
   // init services and subscribers
-  checkPointServer = n.advertiseService("/COMPSCI403/CheckPoint", checkPoint);
-  getFreePathServer = n.advertiseService("/COMPSCI403/GetFreePath", getFreePath);
-  transformPointsServer = n.advertiseService("/COMPSCI403/ObstacleLaserScan", transformPoints);
-  getCommandVelocityServer = n.advertiseService("/COMPSCI403/GetCommandVel", getCommandVelocity);
+  // checkPointServer = n.advertiseService("/COMPSCI403/CheckPoint", checkPoint);
+  // getFreePathServer = n.advertiseService("/COMPSCI403/GetFreePath", getFreePath);
+  // transformPointsServer = n.advertiseService("/COMPSCI403/ObstacleLaserScan", transformPoints);
+  // getCommandVelocityServer = n.advertiseService("/COMPSCI403/GetCommandVel", getCommandVelocity);
+
+  // Blob Detection callback
+  image_transport::ImageTransport it_(n);
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_policy_classification;
+  message_filters::Subscriber<sensor_msgs::Image> image_sub_(n, "/camera/rgb/image_color", 10);
+  message_filters::Subscriber<sensor_msgs::Image> points(n, "/camera/depth/image", 10);
+  message_filters::Synchronizer<sync_policy_classification> sync(sync_policy_classification(10), image_sub_, points);
+  sync.registerCallback(boost::bind(&callback, _1, _2));
 
   laserScanSubscriber = n.subscribe("/COMPSCI403/LaserScan", 10000, laserScanCallback);
   odometrySubscriber = n.subscribe("/odom", 10000, odometeryCallback);
